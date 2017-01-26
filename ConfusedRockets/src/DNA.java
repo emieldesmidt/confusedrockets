2b8206f9f4b94537d43b098e5e749691650c16e7
Lex van der Stoep <lex.vanderstoep@gmail.com>
2017-01-26 12:28:37 +0000

    First start on DNA class

--- ConfusedRockets/src/DNA.java
+++ ConfusedRockets/src/DNA.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 /**
- * Created by Lex on 26/01/2017.
+ * The DNA of the rockets. They represent the route that they will follow.
  */
 public class DNA {
+    private Vector2D[] mGenes;
+
+    public DNA (Vector2D[] genes) {
+        mGenes = genes;
+    }
+    public DNA (int lifeSpan) {
+        // Create a new set of randomised genes
+        Vector2D[] genes = new Vector2D[10*lifeSpan];
+        Arrays.stream(genes).forEach(u-> u = Vector2D.random());
+    }
+
+
 }
