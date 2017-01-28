a29d0f8b919f08bd6559d96b7913efe80128ba6e
Emiel de Smidt <desmidt.emiel@gmail.com>
2017-01-28 17:40:23 +0100

    improved the generation counter. Now also displays statics while running.

--- ConfusedRockets/src/Launch.java
+++ ConfusedRockets/src/Launch.java
@@ -9,13 +9,15 @@ public class Launch {
   public static void main(String[] args) {
     System.out.println("Launching");
     RocketSwarm swarm = new RocketSwarm(100, 200, 10);
-    int genCount = 100;
+    int genCount = 300;
 
-    for (int i = 0; i < genCount; i++) {
+    for (int i = 0; i < genCount + 1; i++) {
       //animate each frame
       for (int j = 0; j < swarm.getSpan(); j++) {
         //swarm.update();
-        j++;
+        System.out.print("Generation:  " + i + "  ( " + 100 * i / genCount + "% )");
+        System.out.write('\r');
+
       }
       //at the end of the population's lifespan, generate a new population.
       swarm.breed(0.01);
