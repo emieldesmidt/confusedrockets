a8163b79d0dd8069e3e021edea32319beabbdb8c
Emiel de Smidt <desmidt.emiel@gmail.com>
2017-02-01 12:21:41 +0100

    Basic gui implementation

--- ConfusedRockets/src/Launch.java
+++ ConfusedRockets/src/Launch.java
@@ -1,12 +1,39 @@
 import ConfusedRockets.RocketSwarm;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.stage.Stage;
+
 
 /**
  * Created by Emiel de Smidt on 27-1-2017.
  * Main class of the application
  */
-public class Launch {
+public class Launch extends Application {
 
   public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Confused Rockets");
+    Group root = new Group();
+    Canvas canvas = new Canvas(1000, 750);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+    root.getChildren().add(canvas);
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+    launch(gc);
+  }
+
+
+
+  public void launch(GraphicsContext gc){
     int span = 100;
 
     System.out.println("Launching");
@@ -19,6 +46,7 @@ public class Launch {
         //swarm.update();
         System.out.print("Generation:  " + i + "  ( " + 100 * i / genCount + "% )");
         System.out.write('\r');
+
 
       }
       //at the end of the population's lifespan, generate a new population.
