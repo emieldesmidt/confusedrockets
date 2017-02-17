f50568f7c18f0ac7d8b12051c9db09f10239624d
Lex van der Stoep <lex.vanderstoep@gmail.com>
2017-02-17 01:37:06 +0000

    Fixed passing through obstacle problem

--- ConfusedRockets/src/ConfusedRockets/Rocket.java
+++ ConfusedRockets/src/ConfusedRockets/Rocket.java
@@ -105,7 +105,7 @@ public class Rocket {
     this.mAcceleration = this.mAcceleration.scale(0);
 
     // If the rocket collides with any of the obstacles, change its status and freeze it
-    if (this.collides(obstacles)) {
+    if (this.collides(obstacles, oldPosition, mPosition)) {
       this.mStatus = RocketStatus.CRASHED;
       this.mPosition = oldPosition;
       this.mVelocity = Vector2D.ZERO;
@@ -129,16 +129,89 @@ public class Rocket {
     return mStatus;
   }
 
-  private boolean collides(List<Rectangle> obstacles) {
+  private static boolean collides(List<Rectangle> obstacles, Vector2D oldPosition, Vector2D newPosition) {
     for (Rectangle r: obstacles) {
-      double x = getPosition().x();
-      double y = getPosition().y();
 
-      // Rocket collides if it is inside an obstacle
-      if (x >= r.getX() && x <= r.getX() + r.getWidth() && y >= r.getY() && y <= r.getY() + r.getHeight()) {
+      // Rocket collides if it its movement hits any of the rectangle boundaries on the way
+      if (intersectRectangle(r, oldPosition, newPosition)) {
         return true;
       }
     }
+
+    return false;
+  }
+
+  // Returns true if the line from the old position to the new position crosses any of the rectangle lines
+  private static boolean intersectRectangle(Rectangle r, Vector2D oldPosition, Vector2D newPosition) {
+    Vector2D leftTop = new Vector2D(r.getX(), r.getY());
+    Vector2D rightTop = new Vector2D(r.getX() + r.getWidth(), r.getY());
+    Vector2D leftBottom = new Vector2D(r.getX(), r.getY() + r.getHeight());
+    Vector2D rightBottom = new Vector2D(r.getX() + r.getWidth(), r.getY() + r.getHeight());
+
+    // The line intersect with rectangle if any of the boundaries of the rectangle crosses it
+    return linesIntersect(leftTop, rightTop, oldPosition, newPosition) |
+           linesIntersect(rightTop, rightBottom, oldPosition, newPosition) |
+           linesIntersect(rightBottom, leftBottom, oldPosition, newPosition) |
+           linesIntersect(leftBottom, leftTop, oldPosition, newPosition);
+  }
+
+  // (c) http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+  private static boolean linesIntersect(Vector2D a1, Vector2D a2, Vector2D b1, Vector2D b2) {
+    // See http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/ for more information
+
+    // Find the four orientations needed for general and special cases
+    int o1 = orientation(a1, a2, b1);
+    int o2 = orientation(a1, a2, b2);
+    int o3 = orientation(b1, b2, a1);
+    int o4 = orientation(b1, b2, a2);
+
+    // General case
+    if (o1 != o2 && o3 != o4) {
+      return true;
+    }
+
+    // Special Cases
+    // a1, a2 and b1 are colinear and b1 lies on segment a1a2
+    if (o1 == 0 && onSegment(a1, b1, a2)) return true;
+
+    // a1, a2 and b1 are colinear and b2 lies on segment a1a2
+    if (o2 == 0 && onSegment(a1, b2, a2)) return true;
+
+    // b1, b2 and a1 are colinear and a1 lies on segment b1b2
+    if (o3 == 0 && onSegment(b1, a1, b2)) return true;
+
+    // b1, b2 and a2 are colinear and a2 lies on segment b1b2
+    if (o4 == 0 && onSegment(b1, a2, b2)) return true;
+
+    return false;
+  }
+
+  // (c) http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+  // To find orientation of ordered triplet (p, q, r).
+  // The function returns following values
+  // 0 --> p, q and r are colinear
+  // 1 --> Clockwise
+  // 2 --> Counterclockwise
+  private static int orientation(Vector2D p, Vector2D q, Vector2D r)
+  {
+    // See http://www.geeksforgeeks.org/orientation-3-ordered-points/
+    // for details of below formula.
+    double val = (q.y() - p.y()) * (r.x() - q.x()) -
+            (q.x() - p.x()) * (r.y() - q.y());
+
+    if (Math.abs(val) < 1E-5) return 0;  // colinear
+
+    return (val > 1E-5)? 1: 2; // clock or counterclock wise
+  }
+
+  // (c) http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+  // Given three colinear points p, q, r, the function checks if
+  // point q lies on line segment 'pr'
+  private static boolean onSegment(Vector2D p, Vector2D q, Vector2D r)
+  {
+    if (q.x() <= Math.max(p.x(), r.x()) && q.x() >= Math.min(p.x(), r.x()) &&
+            q.y() <= Math.max(p.y(), r.y()) && q.y() >= Math.min(p.y(), r.y()))
+      return true;
 
     return false;
   }
